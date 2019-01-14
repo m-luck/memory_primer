bc5d53be9b1fc0707d8ad854f981f21bf43dbafe
Michael <michael.j.lukiman@gmail.com>
2019-01-14 17:21:12 -0500

    Text format successful. Next step: put into graphical grid

---
+++ start_runthrough.py
@@ -0,0 +1,31 @@
+filename = "line_separated_cues.txt"
+file = open(filename, "r")
+
+cues_per_page = 6
+
+cues = []
+pages = []
+page_stats = {}
+cue_stats = {}
+for line in file:
+	phrase = line.split("\n")
+	phrase = phrase[0]
+	cues.append(phrase)
+
+i = 0
+print(len(cues))
+while i < len(cues):
+	page = []
+	for layout_index in range(0,cues_per_page):
+		page.append("")
+		if i < len(cues):
+			page[layout_index] = cues[i]
+		i += 1
+	pages.append(page)
+
+for page in range(0, len(pages)):
+	print("\nPage ",page+1,":")
+	for cue in range(0,cues_per_page):
+		if (cue+1)*(page+1) < len(cues):
+			print(pages[page][cue])
+
