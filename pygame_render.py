7ecc61f020389fe00cb27b0d7ce2f2a9b11ba8cd
Ashkara Vingh II <michael.j.lukiman@gmail.com>
2019-01-22 22:35:37 -0800

    Runthrough works! Next: random and track pause time

--- pygame_render.py
+++ pygame_render.py
@@ -1,28 +1,31 @@
-import request
 import pygame
+import time
 import sys
 import random
 import math
-def launch_graphics(rows,cols,x=500,y=500):
+def launch_graphics(rows,cols,deck,w=500,h=500):
     #initializes video
     pygame.init()
     pygame.font.init()
     font = pygame.font.SysFont('Comic Sans MS', 30)
     #sets the screen size
-    screen = pygame.display.set_mode((x, y))
-    paused = False
+    screen = pygame.display.set_mode((w, h))
+    pause_state = False
     blue=(0,0,255)
     red=(255,0,0)
-    row_height = math.floor(y / rows)
-    col_width = math.floor(x / cols)
+    row_height = math.floor(h / rows)
+    col_width = math.floor(w / cols)
     def eventHandler(pause_state):
+        reset = False
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         pause_state = not pause_state
-        return pause_state
+                    if event.key == pygame.K_r:
+                        reset = True
+        return pause_state, reset
     def drawScreen():
         color = (230, 230, 230)
         screen.fill(color)
@@ -34,21 +37,38 @@ def launch_graphics(rows,cols,x=500,y=500):
         x = x - new_text.get_rect().width/2
         y = y - new_text.get_rect().height/2
         screen.blit(new_text,(x,y))
-    def drawCards(row_height,col_width):
-        text = "beebo"
-        while (1):
-            pause_state = eventHandler(paused)
-            drawScreen()
-            if paused is not True:
-                drawCard(0,text,row_height,col_width,blue)
-            else:
-                drawCard(0,text,row_height,col_width,red)
-            pygame.display.update()
-    def drawCard(cueIndex,text,row_height,col_width,color):
-        card_x = cueIndex * col_width
-        card_y = cueIndex * row_height
+    def drawCard(cueIndex,text,rows,cols,row_height,col_width,color):
+        card_x = ((cueIndex%cols) * col_width)
+        card_y = (math.floor(cueIndex/(rows+1)) * row_height)
         drawRect(color,card_x,card_y,col_width,row_height)
         renderText(text,card_x+col_width/2,card_y+row_height/2,col_width,row_height)
-    drawCards(row_height,col_width)
-
-launch_graphics(2,2)
+    def drawCardGrid(ind,deck,rows,cols,row_height,col_width,color):
+        page = deck[ind]
+        count = 0
+        for cue in page:
+            drawCard(count,cue,rows,cols,row_height,col_width,color)
+            count += 1
+    def drawCards(rows,cols,row_height,col_width,deck,pause_state):
+        start = time.time()
+        ind = 0
+        while (1):
+            time.sleep(0.1)
+            pause_state, reset = eventHandler(pause_state)
+            now = time.time()
+            interval = 1.5
+            if now - start > interval:
+                start = time.time()
+                if ind < len(deck)-1 and pause_state == False:
+                    ind += 1
+            if reset == True:
+                reset = False
+                if ind == len(deck)-1:
+                    ind = 0
+                    pause_state = False
+            drawScreen()
+            if pause_state is not True:
+                drawCardGrid(ind,deck,rows,cols,row_height,col_width,blue)
+            else:
+                drawCardGrid(ind,deck,rows,cols,row_height,col_width,red)
+            pygame.display.update()
+    drawCards(rows,cols,row_height,col_width,deck,pause_state)
--- start_runthrough.py
+++ start_runthrough.py
@@ -3,7 +3,11 @@ from pygame_render import launch_graphics
 filename = "line_separated_cues.txt"
 file = open(filename, "r")
 
-cues_per_page = 6
+screen_w = 1000
+screen_h = 500
+row = 2
+column = 3
+cues_per_page = row*column
 
 cues = []
 pages = []
@@ -11,9 +15,10 @@ page_stats = {}
 cue_stats = {}
 for line in file:
 	phrase = line.split("\n")
-	phrase = phrase
+	phrase = phrase[0]
+	cues.append(phrase)
+print(cues)
 i = 0
-print(len(cues))
 while i < len(cues):
 	page = []
 	for layout_index in range(0,cues_per_page):
@@ -22,11 +27,9 @@ while i < len(cues):
 			page[layout_index] = cues[i]
 		i += 1
 	pages.append(page)
-
-for page in range(0, len(pages)):
-	print("\nPage ",page+1,":")
-	for cue in range(0,cues_per_page):
-		if (cue+1)*(page+1) < len(cues):
-			print(pages[page][cue])
-
-launch_graphics
+# for page in range(0, len(pages)):
+# 	print("\nPage ",page+1,":")
+# 	for cue in range(0,cues_per_page):
+# 		if (cue+1)*(page+1) < len(cues):
+# 			print(pages[page][cue])
+launch_graphics(row,column,pages,screen_w,screen_h)
